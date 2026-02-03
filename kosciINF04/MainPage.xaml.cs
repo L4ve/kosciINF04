3785987ee6635ead58536077e2633f40d844795a
L4ve <pawelskowronski2008@gmail.com>
2026-02-03 18:58:25 +0100

    dodano obrazy zamiast liczb

--- kosciINF04/MainPage.xaml.cs
+++ kosciINF04/MainPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿
-
-namespace kosciINF04
+﻿namespace kosciINF04
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        int roll1 = 0;
-        int roll2 = 0;
-        int roll3 = 0;
-        int roll4 = 0;
-        int roll5 = 0;
-        int wynik = 0;
+        int roll1, roll2, roll3, roll4, roll5;
         int totalSum = 0;
+        Random random = new Random();
+
         public MainPage()
         {
             InitializeComponent();
@@ -19,8 +13,6 @@ namespace kosciINF04
 
         private void OnCounterClicked(object? sender, EventArgs e)
         {
-            var random = new Random();
-
             roll1 = random.Next(1, 7);
             roll2 = random.Next(1, 7);
             roll3 = random.Next(1, 7);
@@ -28,32 +20,28 @@ namespace kosciINF04
             roll5 = random.Next(1, 7);
 
             int wynik = roll1 + roll2 + roll3 + roll4 + roll5;
-
             totalSum += wynik;
 
-            Label1.Text = roll1.ToString();
-            Label2.Text = roll2.ToString();
-            Label3.Text = roll3.ToString();
-            Label4.Text = roll4.ToString();
-            Label5.Text = roll5.ToString();
+            Dice1.Source = $"k{roll1}.jpg";
+            Dice2.Source = $"k{roll2}.jpg";
+            Dice3.Source = $"k{roll3}.jpg";
+            Dice4.Source = $"k{roll4}.jpg";
+            Dice5.Source = $"k{roll5}.jpg";
+
             Label6.Text = "Wynik: " + totalSum;
         }
 
         private void ResetClicked(object? sender, EventArgs e)
         {
-            roll1 = 0;
-            roll2 = 0;
-            roll3 = 0;
-            roll4 = 0;
-            roll5 = 0;
-            wynik = 0;
+            roll1 = roll2 = roll3 = roll4 = roll5 = 0;
             totalSum = 0;
 
-            Label1.Text = "0";
-            Label2.Text = "0";
-            Label3.Text = "0";
-            Label4.Text = "0";
-            Label5.Text = "0";
+            Dice1.Source = "question.jpg";
+            Dice2.Source = "question.jpg";
+            Dice3.Source = "question.jpg";
+            Dice4.Source = "question.jpg";
+            Dice5.Source = "question.jpg";
+
             Label6.Text = "Wynik: 0";
         }
     }
